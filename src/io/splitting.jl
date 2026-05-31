c83333ccc41b929288c5247cb901ebfb16722ca1
mttb91 <63959261+mttb91@users.noreply.github.com>
2026-05-31 17:03:53 +0200

    Enable dataset downsampling
    Allow generating a ML-ready dataset split with less AC-OPF instances than the generated ones. The downsampling logic is implemented at the level of splitting functions:
    - `total_load_active_power_split`
    - `topology_split`

--- src/io/splitting.jl
+++ src/io/splitting.jl
@@ -16,16 +16,20 @@ belonging to a single fold.
 function generate_cv_folds!(map:: _DF.DataFrame, setting::NamedTuple)
 
     n_fold = setting.DATASET.num_folds
+    n_sample = setting.DATASET.num_samples
     rng = _RND.MersenneTwister(setting.CASE.baseseed)
+    @assert n_sample <= size(map, 1) "Number of samples to split cannot be greater than the total number of available samples."
 
     if length(unique(map.topology_id)) == 1
         folds = total_load_active_power_split(
             map,
             rng,
             n_fold,
-            setting.DATASET.num_quantiles)
+            setting.DATASET.num_quantiles;
+            n_sample = n_sample
+        )
     else
-        folds = topology_split(map, rng, n_fold)
+        folds = topology_split(map, rng, n_fold; n_sample = n_sample)
     end
     _DF.insertcols!(map, 2, :fold => folds)
     # Remove unassigned instances from split-specific map in any
@@ -37,28 +41,58 @@ function generate_cv_folds!(map:: _DF.DataFrame, setting::NamedTuple)
 end
 
 "Split samples in CV folds for dataset with multiple topologies"
-function topology_split(map::_DF.DataFrame, rng::_RND.AbstractRNG, n_fold::Int)
-
+function topology_split(
+    map::_DF.DataFrame,
+    rng::_RND.AbstractRNG,
+    n_fold::Int;
+    n_sample::Union{Nothing, Int} = nothing
+)
     _DF.sort!(map, :topology_id)
 
-    ids = _RND.shuffle(rng, unique(map.topology_id))
-    dim = length(copy(ids)) ÷ n_fold
-    folds = zeros(Int, size(map, 1))
-    for i in 1:n_fold
+    if isnothing(n_sample)
+        n_sample = size(map, 1)
+    end
 
-        num = 1:(i == n_fold ? length(ids) : dim)
-        mask = in.(map.topology_id, Ref(ids[num]))
-        folds[mask] .= i
-        deleteat!(ids, num)
+    ids = _RND.shuffle(rng, unique(map.topology_id))
+    topo_idx = Dict(id => findall(==(id), map.topology_id) for id in ids)
+    dim = n_sample ÷ n_fold
+    rem = n_sample % n_fold
+    folds = fill(-1, size(map, 1))
+    for i in 1:n_fold
+        target = dim + (i <= rem ? 1 : 0)
+        assigned = 0
+
+        while assigned < target
+            id = popfirst!(ids)
+            idx = topo_idx[id]
+            num = target - assigned
+
+            if length(idx) <= num
+                folds[idx] .= i
+                assigned += length(idx)
+            else
+                _RND.shuffle!(rng, idx)
+                folds[idx[1:num]] .= i
+                assigned += num
+            end
+        end
     end
     return folds
 end
 
 "Split samples in CV folds for dataset with single topology"
-function total_load_active_power_split(map::_DF.DataFrame, rng::_RND.AbstractRNG, n_fold::Int, n_quantile::Int)
-
+function total_load_active_power_split(
+    map::_DF.DataFrame,
+    rng::_RND.AbstractRNG,
+    n_fold::Int,
+    n_quantile::Int;
+    n_sample::Union{Nothing, Int} = nothing
+)
     _DF.sort!(map, :uid)
     @assert issorted(map.pd_tot)
+
+    # Limit bin size if n_samples is provided
+    bin_size = isnothing(n_sample) ? typemax(Int) : n_sample ÷ n_quantile
 
     # Bin UID based on total load active power quantiles
     r = 1 / n_quantile
@@ -67,15 +101,16 @@ function total_load_active_power_split(map::_DF.DataFrame, rng::_RND.AbstractRNG
     bins = Dict(i => findall(x -> x == i, classes) for i in 1:n_quantile)
 
     # Create folds with stratified sampling
-    folds = zeros(Int, size(map, 1))
+    folds = fill(-1, size(map, 1))
     for bin in values(bins)
 
         _RND.shuffle!(rng, bin)
-        dim = length(bin) ÷ n_fold
+        budget = min(length(bin), bin_size)
+        dim = budget ÷ n_fold
         @assert dim > 0 "Not enough samples to create $n_fold folds with $n_quantile quantiles, reduce either."
 
         for (i, fold) in enumerate(_RND.shuffle(rng, 1:n_fold))
-            num = 1:(i == n_fold ? length(bin) : dim)
+            num = 1:(i == n_fold ? budget - dim * (n_fold - 1) : dim)
             folds[bin[num]] .= fold
             deleteat!(bin, num)
         end
